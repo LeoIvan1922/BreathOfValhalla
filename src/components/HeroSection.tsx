53522e86508b0ac1b78721212e213830c27176b4
LeoIvan1922 <mar.chep95@gmail.com>
2025-11-03 23:35:50 +0200

    Updated HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -38,7 +38,7 @@ export function HeroSection() {
               className="text-lg px-8 py-3 h-auto"
             >
               <Gift className="w-5 h-5 mr-2" />
-              Ознайомтеся з лотереями
+              Ознайомтеся з рафлами
             </Button>
             <Button 
               size="lg" 
